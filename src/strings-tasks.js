4461d7402d687cb92c5ba39aa3b424aa93ae1fc9
Yakubovich-siarhei <yakubovich.siarhei@mail.ru>
2023-11-30 17:28:38 +0300

    feat: 6 task(november 30, 2023 17:00:00 PM)

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -38,8 +38,16 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (value === undefined) return false;
+  if (value === null) return false;
+  if (typeof value === 'boolean') return false;
+  if (typeof value === 'string') return true;
+  if (typeof value !== 'string') {
+    const res = value.valueOf();
+    if (typeof res === 'string') return true;
+  }
+  return !value;
 }
 
 /**
@@ -54,8 +62,8 @@ function isString(/* value */) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  return value1.concat(value2);
 }
 
 /**
@@ -69,8 +77,8 @@ function concatenateStrings(/* value1, value2 */) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  return value.charAt(0);
 }
 
 /**
